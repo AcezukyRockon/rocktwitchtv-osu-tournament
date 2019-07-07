658ece2167ff1e873113db86fef0feb1892ffb5a
rock-on <36922874+fireser@users.noreply.github.com>
2019-07-07 18:29:31 +0700

    Upload codes

--- osu! sheet/Rule.cpp
+++ osu! sheet/Rule.cpp
@@ -12,8 +12,12 @@ void Rule::PrintMenu1()
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
 	cout << "   (just copy and paste the link like you do in normal way)";
 	Base::gotoxy(28, 7);
-	cout << "- Only Final has BO9 format. Everything else are BO7.";
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+	cout << "  This is a private osu! standard 1v1 tourney and will only be hosted for No Title discord server.";
 	Base::gotoxy(28, 9);
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+	cout << "- Only Final has BO9 format. Everything else are BO7.";
+	Base::gotoxy(28, 10);
 	cout << "- A referee will invite you 15 minutes before matches";
 	Base::gotoxy(28, 11);
 	cout << "- You have 10 mins to show up after match starts, if not you will be dq-ed";
